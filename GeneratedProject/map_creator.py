4c15c3e3e7d03282a5e0b049b4ad1545387f5548
swuser <serge.smirnoff@gmail.com>
2023-03-21 16:02:04 +0000

    arg name added

--- GeneratedProject/map_creator.py
+++ GeneratedProject/map_creator.py
@@ -1,4 +1,5 @@
 import re
+import sys
 from bs4 import BeautifulSoup, NavigableString
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
@@ -80,7 +81,6 @@ def set_identifier(identifier):
     global attr_id
     attr_id = identifier
 
-
 def search_and_create(link, identifier, name, mode="w"):
     set_identifier(identifier)
     lookup(link)
@@ -92,7 +92,8 @@ def create_mapper(name: str, mode):
     if mode != "a":
         f.write(f"from selenium.webdriver.common.by import By\n")
         f.write(f"# this is a mapper for page: {address}\n")
-        f.write(f"class {name.capitalize()}:\n")
+        x = name.split('\\')[-1].capitalize()
+        f.write(f"class {x}:\n")
         f.write("\n")
         f.write("\tdef __init__(self, sd):\n\t\tself.sd = sd\n\n")
     for elem in unique_elements:
@@ -115,7 +116,8 @@ def create_mapper(name: str, mode):
         f.write("\n\n")
     f.close()
 
+#search_and_create("https://www.w3schools.com/", "id", "basic_map2", "w")
+search_and_create("https://profile.w3schools.com/log-in?redirect_url=https%3A%2F%2Fmy-learning.w3schools.com", "id", (sys.argv[1:])[0], "w")
 
-# search_and_create("https://www.w3schools.com/", "id", "basic_map", "w")
 # time.sleep(4)
 # search_and_create("http://128.90.13.104:8084/ows-mmr", "class", "login", "a")
